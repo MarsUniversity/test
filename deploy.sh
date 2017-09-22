10fbae2b1f8b2028f18e38a5ea079b179317e2b2
walchko <kevin.walchko@github.noreply.com>
2017-09-22 17:46:04 -0600

    Deploying to github.com/MarsUniversity/test:gh-pages

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# https://github.com/travis-ci/dpl/blob/master/lib/dpl/provider/pages.rb
+
+EMAIL="kevin.walchko@github.noreply.com"
+USER="walchko"
+REPO="github.com/MarsUniversity/test"
+BRANCH="gh-pages"
+TOKEN=$GITHUB_TOKEN
+
+set -e
+
+# rm -rf .git > /dev/null 2>&1
+
+# rm LICENSE
+# rm README.md
+
+echo "hi" > index.html
+
+if [[ -f ".gitignore" ]]; then
+	rm .gitnore
+fi
+
+touch "deployed at `date`"
+# git init
+git config user.email ${EMAIL}
+git config user.name ${USER}
+git add *
+git commit -m "Deploying to ${REPO}:${BRANCH}"
+git push --force --quiet "https://${TOKEN}@${REPO}" master:${BRANCH} > /dev/null 2>&1
